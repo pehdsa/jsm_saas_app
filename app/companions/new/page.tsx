5c1fc9e770ab178feeafd476bd2cec5f276e9dad
Pedro Henrique <pehdsa@gmail.com>
2025-05-24 19:03:54 -0300

    first commit

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewCompanionPage = () => {
+  return <div>New</div>;
+};
+
+export default NewCompanionPage;
---
+++ app/my-journey/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyJourneyPage = () => {
+  return <div>My Journey</div>;
+};
+
+export default MyJourneyPage;
