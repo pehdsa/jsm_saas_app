7be026f82ddb2bc0035c9a8e7eca4b086ad2fe23
Pedro Henrique <pehdsa@gmail.com>
2025-05-24 23:45:28 -0300

    feat: inciada a pagina do compaanios

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,7 +1,59 @@
+import { getCompanion } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const CompanionPage = () => {
-  return <div></div>;
+interface CompanionSessionProps {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+const CompanionPage = async ({ params }: CompanionSessionProps) => {
+  const { id } = await params;
+  const companion = await getCompanion(id);
+  const user = await currentUser();
+
+  if (!user) {
+    redirect("/sign-in");
+  }
+
+  if (!companion) {
+    redirect("/companions");
+  }
+
+  return (
+    <main>
+      <article className="flex rounded-border justify-between p-6 max-md:flex-col">
+        <div className="flex items-center gap-2">
+          <div
+            className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden"
+            style={{ backgroundColor: getSubjectColor(companion.subject) }}
+          >
+            <Image
+              src={`/icons/${companion.subject}.svg`}
+              alt={companion.subject}
+              width={20}
+              height={20}
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center gap-2">
+              <p className="font-bold text-2xl">{companion.name}</p>
+              <p className="subject-badge max-sm:hidden">{companion.subject}</p>
+            </div>
+            <p className="text-lg">{companion.topic}</p>
+          </div>
+        </div>
+        <div className="items-start text-2xl max-md:hidden">
+          {companion.duration} mins
+        </div>
+      </article>
+    </main>
+  );
 };
 
 export default CompanionPage;
---
+++ components/SearchInput.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Image from "next/image";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { formUrlQuery, removeKeysFromUrlQuery } from "@jsmastery/utils";
+
+export const SearchInput = () => {
+  const pathName = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("topic") || "";
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    const delayDebounce = setTimeout(() => {
+      if (searchQuery) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "topic",
+          value: searchQuery,
+        });
+
+        router.push(newUrl);
+      } else {
+        if (pathName === "/companions") {
+          const newUrl = removeKeysFromUrlQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["topic"],
+          });
+
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 700);
+  }, [searchQuery, router, searchParams, pathName]);
+
+  return (
+    <div className="relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit">
+      <Image src="/icons/search.svg" alt="Search Icon" width={15} height={15} />
+      <input
+        placeholder="Search companions..."
+        className="outline-none"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+    </div>
+  );
+};
